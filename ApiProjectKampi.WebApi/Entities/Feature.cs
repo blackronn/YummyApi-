09bdc53407e9851466aa5d76273f8228f8e254bb
blackronn <200101057@ogrenci.yalova.edu.tr>
2025-04-26 16:19:42 +0300

    Add project files.

---
+++ ApiProjectKampi.WebApi/Entities/Feature.cs
@@ -0,0 +1,17 @@
+﻿namespace ApiProjectKampi.WebApi.Entities
+{
+    public class Feature
+    {
+        public int FeatureID { get; set; }
+        public string Title { get; set; }
+
+        public string Subtitle { get; set; }
+
+        public string Description { get; set; }
+
+        public string VideoUrl { get; set; }
+
+        public string ImageUrl { get; set; }
+
+    }
+}
---
+++ ApiProjectKampi.WebApi/Entities/Reservation.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiProjectKampi.WebApi.Entities
+{
+    public class Reservation
+    {
+        public int ReservationId { get; set; }
+        public int NameSurname { get; set; }
+        public int Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public String ReservationTime { get; set; }
+        public int CountOfPeople { get; set; }
+        public string Message { get; set; }
+        public bool ReservationStatus { get; set; }
+    }
+}
